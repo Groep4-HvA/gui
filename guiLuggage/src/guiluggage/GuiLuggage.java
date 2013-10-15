5ff2da777c47fba96aec5f343b8b1686bd2b559f
Sean Molenaar <smillerdev@me.com>
2013-10-15 14:52:06 +0200

    Added a temporary main form

--- guiLuggage/src/guiluggage/GuiLuggage.java
+++ guiLuggage/src/guiluggage/GuiLuggage.java
@@ -10,6 +10,7 @@ public class GuiLuggage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainGuiFram main = new MainGuiFram();
+        main.setVisible(true);
     }
 }
