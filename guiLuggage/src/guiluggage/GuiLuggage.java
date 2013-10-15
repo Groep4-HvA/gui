17cae5fe21236387b52f46cb12778469aa63b9aa
smillernl <smillernl@localhost.localdomain>
2013-10-15 23:31:24 +0200

    Translation? not so much

--- guiLuggage/src/guiluggage/GuiLuggage.java
+++ guiLuggage/src/guiluggage/GuiLuggage.java
@@ -1,5 +1,8 @@
 package guiluggage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  *
  * @author groep 4
@@ -10,7 +13,25 @@ public class GuiLuggage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Locale currentLocale;
+        ResourceBundle messages;
+        String language;
+        String country;
+        
+//        if (args.length != 2) {
+            language = "en";
+            country = "US";
+//        } else {
+//            language = args[0];
+//            country = args[1];
+//        }
         MainGuiFrame main = new MainGuiFrame();
+        currentLocale = new Locale(language, country);
+                
+        messages = ResourceBundle.getBundle("translation", currentLocale);
+        //main.setName(messages.getString("greetings"));
+        System.out.print(messages.getString("greetings"));
         main.setVisible(true);
+        
     }
 }
