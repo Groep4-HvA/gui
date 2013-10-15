81e434efc9862fc7e0ac20d609938400834ca8d2
Sean Molenaar <smillerdev@me.com>
2013-10-15 11:04:18 +0200

    Initial commit

---
+++ guiLuggage/src/guiluggage/GuiLuggage.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guiluggage;
+
+/**
+ *
+ * @author sean
+ */
+public class GuiLuggage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
