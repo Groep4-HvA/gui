bbfe267d26ea76eee3a11fd7698d9cf17d2b50e4
zhengh001 <hfzheng.bs@gmail.com>
2013-11-28 14:22:37 +0100

    Addluggage

--- guiLuggage/src/models/Luggage.java
+++ guiLuggage/src/models/Luggage.java
@@ -9,5 +9,59 @@ package models;
  * @author ChrisvanderHeijden
  */
 public class Luggage {
+    private String label;
+    private String color;
+    private String shape;
+    private String location;
+    private String details;
+
+    public Luggage(String label, String color, String shape, String location, String details) {
+        this.label = label;
+        this.color = color;
+        this.shape = shape;
+        this.location = location;
+        this.details = details;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
     
 }
