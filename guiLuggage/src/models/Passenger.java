17c324f2c2776cb8a3e0f34d1f66c871e87ef404
zhengh001 <hfzheng.bs@gmail.com>
2013-11-28 10:57:36 +0100

    Passenger.java gettersetters waren verwijderd

--- guiLuggage/src/models/Passenger.java
+++ guiLuggage/src/models/Passenger.java
@@ -32,7 +32,85 @@ public class Passenger {
         this.city = city;
         this.details = details;
     }
-    
-    
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getResidentAdres() {
+        return residentAdres;
+    }
+
+    public void setResidentAdres(String residentAdres) {
+        this.residentAdres = residentAdres;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
     
 }
